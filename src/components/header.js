a76165003fe9fe4521c0b187315b72e210400687
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-10-01 10:16:10 -0500

    connecting to codegrade

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,33 @@
 const Header = (title, date, temp) => {
+
+  // Initialize 
+
+  const header = document.createElement('div')
+  const articleDate = document.createElement('span')
+  const articleTitle = document.createAttribute('h1')
+  const todayTemp = document.createAttribute('span')
+
+  // Add classes
+
+  header.classList.add('header');
+  articleDate.classList.add('date');
+  // todayTemp.classList.add('temp');
+
+  // Add Text Content
+
+  articleDate.textContent = date
+  articleTitle.textContent = title
+  todayTemp.textContent = temp
+
+  // Structuring
+
+  header.appendChild(articleDate)
+  header.appendChild(articleTitle)
+  header.appendChild(todayTemp)
+
+  console.log(header);
+  return header;
+
   // TASK 1
   // ---------------------
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
@@ -20,6 +49,9 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+
+  const newHeader = Header("Zebra on Moon!", "October 31st", "100 F");
+  document.querySelector(selector).appendChild(newHeader);
 }
 
 export { Header, headerAppender }
